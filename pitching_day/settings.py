103cf3d8ab42302e37a2c362dfed1a990ca26482
Elhadi <mohamed.elhadi.bachir@ensia.edu.dz>
2025-11-19 21:19:12 +0100

    changes

--- pitching_day/settings.py
+++ pitching_day/settings.py
@@ -24,10 +24,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list(
-    "ALLOWED_HOSTS",
-    default=["jury.algeria20.com"],
-)
+ALLOWED_HOSTS = ["*"]
 
 # -------------------------------------------------
 # Applications
